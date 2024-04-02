5a3461adc378c5cdfd0d4b645a9ed81de6f77bdd
eanesparrago <eanesparrago@gmail.com>
2024-04-02 17:43:06 +0800

    Add Blog page design

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import styles from './blog.module.css'
+import PostCard from '@/components/postCard/PostCard'
 
-export default function BlogPage() {
+export default function BlogPage () {
+  const testPost = {
+    img: 'https://images.pexels.com/photos/18254876/pexels-photo-18254876/free-photo-of-waves-by-the-rocky-beach.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    createdAt: 'createdAt',
+    title: 'title',
+    body: 'body',
+    post: 'slug'
+  }
+
   return (
-    <div>BlogPage</div>
+    <div className={styles.container}>
+      <div className={styles.post}>
+        <PostCard post={testPost} />
+      </div>
+      <div className={styles.post}>
+        <PostCard post={testPost} />
+      </div>
+      <div className={styles.post}>
+        <PostCard post={testPost} />
+      </div>
+      <div className={styles.post}>
+        <PostCard post={testPost} />
+      </div>
+      <div className={styles.post}>
+        <PostCard post={testPost} />
+      </div>
+    </div>
   )
 }
