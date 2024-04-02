243bcc78314ae8862a2836001fe47880353017a7
eanesparrago <eanesparrago@gmail.com>
2024-04-02 17:51:45 +0800

    Add single blog page design

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import styles from './singlePost.module.css'
+import Image from 'next/image'
 
-export default function SingleBlogPage() {
+export default function SingleBlogPage () {
   return (
-    <div>SingleBlogPage</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image
+          src="https://images.pexels.com/photos/18254876/pexels-photo-18254876/free-photo-of-waves-by-the-rocky-beach.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'"
+          alt=''
+          fill
+          className={styles.img}
+        />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>title</h1>
+        <div className={styles.detail}>
+          userid
+          <div className={styles.detailText}>
+            <span className={styles.detailTitle}>Published</span>
+            <span className={styles.detailValue}>createdAt</span>
+          </div>
+        </div>
+        <div className={styles.content}>desc</div>
+      </div>
+    </div>
   )
 }
